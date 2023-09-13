7caf138ebf4c23a41ce6978cace313493cbb4cc8
Jason <jason0101@estudiantec.cr>
2023-09-13 00:27:55 -0600

    Se logra generar una linea cuando se seleccionan 2 puntos

--- src/main/java/com/example/connectdots/Main.java
+++ src/main/java/com/example/connectdots/Main.java
@@ -5,18 +5,26 @@ package com.example.connectdots;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Punto puntoSeleccionado = null; // Punto que el jugador está conectando
+    private Line lineaActual = null; // Línea que el jugador está dibujando
+
+    private Pane root; // Declarar la variable root aquí
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Connect_Dots");
-        Pane root = new Pane();
+        primaryStage.setTitle("Mi Juego");
+        root = new Pane(); // Inicializar la variable root aquí
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
 
@@ -28,7 +36,52 @@ public class Main extends Application {
 
         root.getChildren().addAll(punto1, punto2, punto3, punto4);
 
+        scene.setOnMouseClicked(event -> {
+            if (puntoSeleccionado == null) {
+                // El jugador selecciona el primer punto
+                puntoSeleccionado = encontrarPuntoClic(event.getX(), event.getY());
+            } else {
+                // El jugador selecciona el segundo punto
+                Punto segundoPunto = encontrarPuntoClic(event.getX(), event.getY());
+                if (segundoPunto != null && !segundoPunto.equals(puntoSeleccionado)) {
+                    // Conectar los puntos con una línea
+                    Line linea = new Line(puntoSeleccionado.getCenterX(), puntoSeleccionado.getCenterY(),
+                            segundoPunto.getCenterX(), segundoPunto.getCenterY());
+                    root.getChildren().add(linea); // Agregar la línea al Pane
+                    lineaActual = linea;
+
+                    // Reiniciar la selección de puntos
+                    puntoSeleccionado = null;
+                }
+            }
+        });
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    private Punto encontrarPuntoClic(double x, double y) {
+        for (var node : root.getChildren()) {
+            if (node instanceof Punto) {
+                Punto punto = (Punto) node;
+                if (punto.contains(x, y) && !punto.conectado) {
+                    return punto;
+                }
+            }
+        }
+        return null;
+    }
+
+    private class Punto extends Circle {
+        private boolean conectado = false; // Indicador de si el punto ya está conectado
+
+        public Punto(double x, double y) {
+            super(5, javafx.scene.paint.Color.BLACK); // Tamaño y color de los puntos
+            setCenterX(x);
+            setCenterY(y);
+        }
+    }
 }
+
+
 
