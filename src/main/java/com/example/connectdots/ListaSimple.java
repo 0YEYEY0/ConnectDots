6ff95ee658f8dab5b4bfc28373046cc0d0d6b026
Jason <jason0101@estudiantec.cr>
2023-09-26 17:52:41 -0600

    Creo una lista simple para crear la matriz de puntos

---
+++ src/main/java/com/example/connectdots/ListaSimple.java
@@ -0,0 +1,45 @@
+package com.example.connectdots;
+public class ListaSimple<T> {
+    private Nodo<T> cabeza;
+    private int tamaño;
+
+    public ListaSimple() {
+        cabeza = null;
+        tamaño = 0;
+    }
+
+    public void agregar(T elemento) {
+        Nodo<T> nuevoNodo = new Nodo<>(elemento);
+        nuevoNodo.siguiente = cabeza;
+        cabeza = nuevoNodo;
+        tamaño++;
+    }
+
+    public T obtener(int indice) {
+        if (indice < 0 || indice >= tamaño || cabeza == null) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango");
+        }
+
+        Nodo<T> nodoActual = cabeza;
+        for (int i = 0; i < indice; i++) {
+            nodoActual = nodoActual.siguiente;
+        }
+
+        return nodoActual.valor;
+    }
+
+    public int tamaño() {
+        return tamaño;
+    }
+
+    private class Nodo<E> {
+        E valor;
+        Nodo<E> siguiente;
+
+        Nodo(E valor) {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+    }
+}
+
